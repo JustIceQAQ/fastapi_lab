d52bc2a7d6dbf0a9f50e2a8f6e6ef30b0bd9fba2
justiceqaq <justiceqaq@gmail.com>
2023-01-02 22:55:05 +0800

    add: irises ws case

--- apps/irises/api.py
+++ apps/irises/api.py
@@ -1,11 +1,21 @@
 from typing import List
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, WebSocket, Request
+from fastapi.templating import Jinja2Templates
+
+from starlette.websockets import WebSocketDisconnect
 
 from apps.irises import schemas, crud
+from apps.irises.ws import IrisesConnectionManager
 from db.database import get_db
 
 irises_router = APIRouter(prefix="/irises", tags=["Irises"])
+irises_manager = IrisesConnectionManager()
+
+
+def get_all_irises():
+    db = next(get_db())
+    return [schemas.Irises.from_orm(iris).dict() for iris in crud.get_irises(db)]
 
 
 @irises_router.get(
@@ -29,7 +39,9 @@ async def get_iris(iris_id: int, db: Session = Depends(get_db)):
     response_model=schemas.Irises,
     status_code=status.HTTP_201_CREATED)
 async def post_iris(iris: schemas.IrisBase, db: Session = Depends(get_db)):
-    return crud.create_iris(db, iris.dict())
+    data = crud.create_iris(db, iris.dict())
+    await irises_manager.broadcast(get_all_irises())
+    return data
 
 
 @irises_router.put(
@@ -37,7 +49,9 @@ async def post_iris(iris: schemas.IrisBase, db: Session = Depends(get_db)):
     response_model=schemas.Irises,
     status_code=status.HTTP_200_OK)
 async def put_iris(iris_id: int, iris_data: schemas.IrisBase, db: Session = Depends(get_db)):
-    return crud.update_iris(db, iris_id=iris_id, iris_data=iris_data.dict())
+    data = crud.update_iris(db, iris_id=iris_id, iris_data=iris_data.dict())
+    await irises_manager.broadcast(get_all_irises())
+    return data
 
 
 @irises_router.patch(
@@ -45,11 +59,34 @@ async def put_iris(iris_id: int, iris_data: schemas.IrisBase, db: Session = Depe
     response_model=schemas.Irises,
     status_code=status.HTTP_200_OK)
 async def patch_iris(iris_id: int, iris_data: schemas.PatchIris, db: Session = Depends(get_db)):
-    return crud.update_iris(db, iris_id=iris_id, iris_data=iris_data.dict(exclude_unset=True))
+    data = crud.update_iris(db, iris_id=iris_id, iris_data=iris_data.dict(exclude_unset=True))
+    await irises_manager.broadcast(get_all_irises())
+    return data
 
 
 @irises_router.delete(
     "/{iris_id}",
     status_code=status.HTTP_204_NO_CONTENT)
 async def delete_iris(iris_id: int, db: Session = Depends(get_db)):
-    return crud.delete_iris(db, iris_id=iris_id)
+    data = crud.delete_iris(db, iris_id=iris_id)
+    await irises_manager.broadcast(get_all_irises())
+    return data
+
+
+templates = Jinja2Templates(directory="templates")
+
+
+@irises_router.get("/farm/")
+async def farm_iris(request: Request):
+    return templates.TemplateResponse("farm.html", {"request": request})
+
+
+@irises_router.websocket("/ws/")
+async def websocket_endpoint(websocket: WebSocket):
+    await irises_manager.connect(websocket)
+    try:
+        while True:
+            await irises_manager.broadcast(get_all_irises())
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        irises_manager.disconnect(websocket)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from apps.routers import root_routers
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 app.include_router(root_routers)
+app.mount("/static", StaticFiles(directory="static"), name="static")
